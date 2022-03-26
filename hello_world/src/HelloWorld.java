00aae09f22d022f3d8b819a51057e09676ba83c4
Vagner Matheus Da Cruz <79582405+vagnercruz@users.noreply.github.com>
2022-03-26 06:11:32 -0300

    Add files via upload
    This is Hello World !!!

---
+++ hello_world/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+    public void sayHello(){
+        System.out.println("Hello World!");
+        System.out.println("Este é o meu primeiro programa em Java!");
+    }
+}
---
+++ hello_world/src/Program.java
@@ -0,0 +1,19 @@
+public class Program {
+
+    public static void main(String[] args) {
+//      Tipo       nome do objeto          Nome da Classe
+        HelloWorld helloWorld        = new HelloWorld();
+        helloWorld.sayHello();
+    }
+}
+
+
+/*
+    O tipo vai sempre ter o nome da classe
+
+    O nome do objeto pode ser qualquer um, mas eu decidir por o  mesmo da classe porém seguindo o camel case (a primeira minuscula)
+
+    = é o sinal de atribuição
+
+    HelloWorld() é o nome da classe, ou seja estamos chamando o constructor
+*/
